663908421b773d7194ff4f4828df794e93cf17dd
Chocoffees <sirogrenadine@gmail.com>
2022-02-26 06:27:05 +0100

    Update global app logic: products routes

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,8 @@ const express = require('express');
 // Import Mongoose
 const mongoose = require('mongoose');
 
-// Import model 'sauce'
-const sauce = require('./models/sauce');
+// Import router 'sauce'
+const sauceRoutes = require('./routes/sauce');
 
 require('dotenv').config()
 // Create Express application
@@ -32,39 +32,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/api/sauces', (req, res, next) => {
-  sauce.find() // return all sauces [] from database 
-  .then(sauces => res.status(200).json(sauces)) // Status 200 = success
-  .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauces/:id', (req, res, next) => {
-  sauce.findOne({ _id: req.params.id }) // return 1 sauce (match id)
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-}); 
-
-app.post('/api/sauces', (req, res, next) => {
-  delete req.body._id;
-    const sauce = new Sauce({
-    ...req.body,
-    });
-    sauce.save() // save new sauce in database 
-    .then(() => res.status(201).json({ message: 'Nouvelle sauce créée !'})) // Status 201 = created
-    .catch(error => res.status(400).json({ error })); // Status 400 = bad request
-});
-
-app.put('/api/sauces/:id', (req, res, next) => {
-  sauce.updateOne({ _id: req.params.id}, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce mise à jour !'}))
-    .catch(error => res.status(400).json({ error })); 
-});
-
-app.delete('/api/sauces/:id', (req, res, next) => {
-  sauce.deleteOne({ _id: req.params.id })
-  .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-  .catch(error => res.status(400).json({ error }));
-});
+// In this version: logic routing has been transfered to /routes/sauce.js
+app.use('/api/sauces', sauceRoutes);
 
 // Export the application
 module.exports = app;
