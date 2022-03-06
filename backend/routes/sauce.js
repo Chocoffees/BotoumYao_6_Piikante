bfd770eae35714ce480f8e2a071e507f93b80334
Chocoffees <sirogrenadine@gmail.com>
2022-03-06 18:21:43 +0100

    Add user/routes authentication

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -4,27 +4,30 @@ const express = require('express');
 // Create Express router > set up the routes to be used
 const router = express.Router();
 
+// Import authentication middleware > protect routes
+const auth = require('../middleware/auth');
+
 // Import controller
 const sauceCtrl = require('../controllers/sauce');
 
 // -- Create routes --
 // Get all sauces
-router.get('/', sauceCtrl.getAllSauces);
+router.get('/', auth, sauceCtrl.getAllSauces);
 
 // Get one sauce
-router.get('/:id', sauceCtrl.getOneSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 
 // Create sauce
-router.post('/', sauceCtrl.createSauce);
+router.post('/', auth, sauceCtrl.createSauce);
 
 // Update one sauce
-router.put('/:id', sauceCtrl.updateSauce);
+router.put('/:id', auth, sauceCtrl.updateSauce);
 
 // Delete one sauce
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 // Implement like and dislike status user
-router.post('/:id/like', sauceCtrl.defineLikeStatus);
+router.post('/:id/like', auth, sauceCtrl.defineLikeStatus);
 
 // Export the router
 module.exports = router;
