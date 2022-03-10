e10fcc690499419f139d9cdcccc6975e68a0a1e3
Chocoffees <sirogrenadine@gmail.com>
2022-03-11 00:09:42 +0100

    Signup function review

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -18,9 +18,13 @@ exports.signup = (req, res, next) => {
     });
     user.save()
         .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
+        .catch(error => {
+          console.log(error)
+          res.status(400).json({ error })});
     })
-    .catch(error => res.status(500).json({ error }));
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ error })});
     };
       
 // Connect users to app: use login function to check user informations
